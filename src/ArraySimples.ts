96f9f190282a1a0af3587bd83612e343da8bafc4
Paula Melo <paulamelo2404@gmail.com>
2025-09-10 18:41:32 -0300

    Delete src/ArraySimples.ts

--- src/ArraySimples.ts
+++
@@ -1,7 +0,0 @@
-export class ArraySimples<T> {
-    constructor(private array: T[]) {}
-
-    removeDuplicados(): T[] {
-        return this.array.filter((item, index) => this.array.indexOf(item) === index);
-    }
-}
